a2271aec8021ced29abca66676c3ed43e599e6b0
David German <42500275+GermanDavid@users.noreply.github.com>
2023-12-31 13:49:59 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,17 @@
 import java.util.concurrent.ThreadLocalRandom;
 public class GenThree {
     public static void main(String[] args) {
-		int max,min = 0;
+		/* Feedback
+		The init of max and min in the same line can be misleading. It seems like both are set to 0, but only min is. It's better to init them in seperate lines for clarity
+		int max; 
+		int min = 0;
+		*/
+		int max,min = 0; 
 		min = Integer.parseInt(args[0]);
 		max = Integer.parseInt(args[1]);
+		/* Feedback
+		The variable names firstR, secR, and thirdR are somewhat clear but could be more descriptive, like firstRandom, secondRandom, and thirdRandom.
+		*/
         int firstR = ThreadLocalRandom.current().nextInt(min, max);//getting a random number within the range
 		int secR = ThreadLocalRandom.current().nextInt(min, max);
 		int thirdR = ThreadLocalRandom.current().nextInt(min, max);
@@ -23,3 +31,4 @@ public class GenThree {
 			}
     }
 }
+
